05fbc17b3299815c30421063b6ac70f5bec56071
Mart-1 <martin.lobel@isen-ouest.yncrea.fr>
2023-05-15 14:41:27 +0200

    .CSV Complet + Rectification err3ur du script

--- script.sh
+++ script.sh
@@ -2,15 +2,22 @@
 #Création du dossier shared
 if [ ! -d /home/shared ]; then
     mkdir -p /home/shared
+	echo "Fichier Shared crée"
 fi
 chmod 755 /home/shared
-while IFS=";" read -r name surname mail passwd; do
-	#Login + Suppression des espaces et des \n
-	login="${name:0:1}$surname"
-	login=$(echo "$login" | sed 's/[[:space:]]//g')
-	login=$(echo "$login" | sed 's/\r//g')
-	
-	password=$(echo "$passwd" | sed -e 's/\r//g')
 
-	echo -e "$login - $password"
+while IFS=';' read -r name surname mail passwd; do
+    # Suppression des espaces dans les champs
+    name=$(echo "$name" | sed 's/ //g')
+    surname=$(echo "$surname" | sed 's/ //g')
+    mail=$(echo "$mail" | sed 's/ //g')
+    passwd=$(echo "$passwd" | sed 's/ //g')
+
+    #Login
+    login=${name:0:1}$surname
+
+    password=$(echo "$passwd" | sed -e 's/\r//g')
+
+    echo -e "$login - $password"
 done < <(tail -n +2 accounts.csv)
+
